0fb659a6456afbb8cd3c6c1fd4f8e45923105933
Laith Issa <w0293630@myscc.ca>
2026-03-15 01:06:21 -0400

    add pwa support and offline cache

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 
 const CACHE_NAME = "mushafi-v1";
 const FILES_TO_CACHE = [
-  "/index.html",
-  "/manifest.json"
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon.png"
 ];
 
 self.addEventListener("install", event => {
